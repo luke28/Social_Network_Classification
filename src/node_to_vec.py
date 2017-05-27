0d81ed050485eb15365d225e0cb7bf95e03441c8
luke28 <dulun2834@126.com>
2017-05-27 19:43:17 +0800

    Finish coding the graph structure

---
+++ src/node_to_vec.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+
+class Graph(object):
+    def __init__(self, n):
+        self.n = n
+        self.m = 0
+#each element is a numpy array, save edge features
+        self.edge_set = []
+        self.edge_table = [[]] * n
+    
+    def add_edge(u, v, feas):
+        edge_table[u].append((v, m))
+        edge_set.append((u, v, np.array(feas, dtype = np.float32)))
+        m += 1
+
+    def get_from(Id):
+        return edge_set[Id][0]
+    def get_to(Id):
+        return edge_set[Id][1]
+    def get_edge_feature(Id):
+        return edge_set[Id][2]
+
+def construct_tensorflow_graph():
+    #to be continued
+
+# x_node is a node index vector of the center node neighbours
+# x_edge is a edge index dictionary of the center node neighbors, the key is edge id and the value is a feature vector
