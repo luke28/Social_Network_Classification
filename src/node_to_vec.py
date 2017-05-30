14ceec51d06e1930a2156ca6f563d693d8e2795f
luke28 <dulun2834@126.com>
2017-05-30 23:38:36 +0800

    design a framework

--- src/node_to_vec.py
+++ src/node_to_vec.py
@@ -2,26 +2,47 @@ import numpy as np
 import tensorflow as tf
 
 class Graph(object):
-    def __init__(self, n):
-        self.n = n
-        self.m = 0
+	def __init__(self, n):
+		self.n = n
+		self.m = 0
 #each element is a numpy array, save edge features
-        self.edge_set = []
-        self.edge_table = [[]] * n
-    
-    def add_edge(u, v, feas):
-        edge_table[u].append((v, m))
-        edge_set.append((u, v, np.array(feas, dtype = np.float32)))
-        m += 1
+		self.edge_set = []
+		self.edge_table = [[]] * n
+	
+	def add_edge(u, v, feas):
+		edge_table[u].append((v, m))
+		edge_set.append((u, v, np.array(feas, dtype = np.float32)))
+		m += 1
 
-    def get_from(Id):
-        return edge_set[Id][0]
-    def get_to(Id):
-        return edge_set[Id][1]
-    def get_edge_feature(Id):
-        return edge_set[Id][2]
+	def get_from(Id):
+		return edge_set[Id][0]
+	def get_to(Id):
+		return edge_set[Id][1]
+	def get_edge_feature(Id):
+		return edge_set[Id][2]
 
-def construct_tensorflow_graph():
-    
-# x_node is a node index vector of the center node neighbours
-# x_edge is a edge index dictionary of the center node neighbors, the key is edge id and the value is a feature vector
+class NodeToVec(Object):
+	def __init__(self, g, param):
+		self.tensor_graph = tf.Graph()
+		self.embedding_size = param["embedding_size"]
+		self.feature_size = g.get_edge_feature(0).size
+		self.num_node = g.n
+		self.num_edge = g.m
+		
+		#construct tensorflow graph
+		with tensor_graph.as_default():
+			x_nodes = tf.placeholder(tf.float32, [None, num_node * embedding_size])
+			embeddings = tf.Variable(
+				tf.random_uniform([num_node * embedding_size], -1.0, 1.0))
+			
+			embededs = tf.multiply(embeddings, x_nodes)
+			
+			x_edges = tf.placeholder(tf.float32, [None, num_edges * feature_size])
+			w_edges = tf.Variable(
+				tf.random_uniform([feature_size], -1.0, 1.0))
+		
+	
+	
+	
+# x_nodes is a node index vector of the center node neighbours
+# x_edges is a edge index dictionary of the center node neighbors, the key is edge id and the value is a feature vector
